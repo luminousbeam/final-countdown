7f6bea0e74920e050d9807af08a53cb724dfd735
luminousbeam <jocelyn.jeriah@gmail.com>
2015-05-01 10:17:10 -0400

    w07d05 quiz attempt

---
+++ counter.js
@@ -0,0 +1,33 @@
+//1. Print the current value of the counter to the console.
+console.log(document.getElementById("counter").value)
+
+if (sessionStorage.getItem("is_reloaded")) {
+  console.log("A second passed!");
+}
+
+//was looking up a way to add a hidden counter in jquery sophilabs.github.io/jquery-counter/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.getElementById("counter").value = 1
+
+
+//parseInt(counter, 10) example to change from string to digit
